65363a3f842ad9588aff02290487ec9c24da38ff
nika-choi <156655615+nika-choi@users.noreply.github.com>
2024-02-23 00:38:01 +0200

    Update app.py Division
    Division operation added to calculator

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ def add(a, b):
     return a + b
 def subtract(a, b):
     return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
 select = int(input())
  
 number_1 = int(input("first number: "))
@@ -14,3 +18,10 @@ if select == 1:
 elif select == 2:
     print(number_1, "-", number_2, "=",
                     subtract(number_1, number_2))
+
+elif select == 3:
+    print(number_1, "*", number_2, "=",
+                    multiply(number_1, number_2))
+elif select == 4:
+    print(number_1, "/", number_2, "=",
+                    divide(number_1, number_2))
