8afad4beb5a571d06b22ecaf6ab19c0dcacfe09d
nika-choi <156655615+nika-choi@users.noreply.github.com>
2024-02-23 00:18:45 +0200

    Create app.py
    Created calculator with two types of operations - adding and subtracting

---
+++ app.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+select = int(input())
+ 
+number_1 = int(input("first number: "))
+number_2 = int(input("second number: "))
+ 
+if select == 1:
+    print(number_1, "+", number_2, "=",
+                    add(number_1, number_2))
+ 
+elif select == 2:
+    print(number_1, "-", number_2, "=",
+                    subtract(number_1, number_2))
